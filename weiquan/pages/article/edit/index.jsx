1f733f9d9e14df0856d5b6d0b41a2b17ff7c2648
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-12-17 19:55:38 +0800

    fixed upload png

--- weiquan/pages/article/edit/index.jsx
+++ weiquan/pages/article/edit/index.jsx
@@ -60,13 +60,13 @@ class ArticleEdit extends React.Component {
       })
     }
     beforeUpload (file) {
-      const isJPG = file.type === 'image/jpeg'
+      const isJPG = file.type === 'image/jpeg' || file.type === 'image/png'
       if (!isJPG) {
-        message.error('You can only upload JPG file!')
+        Message.error('You can only upload JPG OR PNG file!')
       }
       const isLt2M = file.size / 1024 / 1024 < 2
       if (!isLt2M) {
-        message.error('Image must smaller than 2MB!')
+        Message.error('Image must smaller than 2MB!')
       }
       return isJPG && isLt2M
     }
